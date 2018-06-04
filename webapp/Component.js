1264dd92eaf03eced860cbff4e95462ca30d6fd2
Jeff Dederick <jdederick@ketch.partners>
2018-06-04 15:59:47 -0400

    added module path

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,15 @@
-sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent){
-	"use strict";
+sap.ui.define(['sap/ui/core/UIComponent'], function(UIComponent) {
+	'use strict';
 	return UIComponent.extend('sap.ui.demo.todo.Component', {
 		metadata: {
-			manifest: "json"
+			manifest: 'json'
 		}
 	});
 });
+
+jQuery.sap.registerModulePath(
+	'openui5-masterdetail-app',
+	'../../../openui5-masterdetail-app/'
+);
+
+jQuery.sap.require('openui5-masterdetail-app.dist.Component');
